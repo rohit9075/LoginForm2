d1f9fce8bc85170a02e5c07c33125af2a7045388
Rohit <rohit9075@gmail.com>
2018-04-14 19:59:57 +0530

    Initial commit

---
+++ app/src/main/java/rohit/com/loginform2/PreLoginActivity.java
@@ -0,0 +1,40 @@
+package rohit.com.loginform2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PreLoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button mLoginbutton, mRegisterButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pre_login);
+
+        mLoginbutton= findViewById(R.id.button2);
+        mRegisterButton = findViewById(R.id.button3);
+
+        mLoginbutton.setOnClickListener(this);
+        mRegisterButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+     switch (v.getId()){
+         case R.id.button2 :
+             Intent intent = new Intent(this, MainActivity.class);
+             startActivity(intent);
+             break;
+         case R.id.button3:
+             Intent intent1 = new Intent(this, UserRegistration.class);
+             startActivity(intent1);
+
+
+     }
+    }
+}
---
+++ app/src/main/java/rohit/com/loginform2/UserRegistration.java
@@ -0,0 +1,13 @@
+package rohit.com.loginform2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_registration);
+    }
+}
